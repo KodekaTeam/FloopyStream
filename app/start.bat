1cf71598caa890c8ab98fcbe000ee1f3941b9078
KodekaTeam <kodekateam@gmail.com>
2025-12-01 20:52:04 +0700

    chore: remove unused script

--- app/start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Start MediaFlow Live Server from correct directory
-
-cd /d D:\KINGJS\floopystream\app
-echo Starting MediaFlow Live from correct directory...
-echo Working directory: %CD%
-echo.
-node server.js
